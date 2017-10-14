e7bc6d6bf8aa5bb9f3f731551a795d26fa317069
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2017-10-14 18:02:46 +0800

    added service UploadImageService

---
+++ services/UploadImageService.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cheremhovo
+ * Date: 14.10.17
+ * Time: 16:15
+ */
+
+namespace cheremhovo\services;
+
+use cheremhovo\fileSystem\fileSystem\File;
+use cheremhovo\fileSystem\fileSystem\Path;
+use DomainException;
+use Imagine\Image\ManipulatorInterface;
+use yii\helpers\ArrayHelper;
+use yii\imagine\Image;
+use yii\web\UploadedFile;
+
+class UploadImageService
+{
+    /**
+     * @var Path
+     */
+    private $path;
+    /**
+     * @var array
+     */
+    private $thumbs;
+
+    /**
+     * UploadImageService constructor.
+     * @param Path $path
+     * @param array $thumbs
+     */
+    public function __construct(Path $path, array $thumbs)
+    {
+        $this->path = $path;
+        $this->thumbs = $thumbs;
+    }
+
+
+    /**
+     * @param string $name
+     */
+    public function run(string $name)
+    {
+        $file = UploadedFile::getInstanceByName($name);
+        if (is_null($file)) {
+            $this->domainException('Нет файла');
+        }
+        $name = (new File($file->name))->generateName();
+        $this->path->setFile(new File($name));
+
+        if (!$file->saveAs($this->path->getPathFile())) {
+            $this->domainException('Ошибка сохранение файла');
+        }
+
+        if (!$this->path->isExist()) {
+            $this->domainException('Нет файла');
+        }
+
+        $this->createThumbs();
+    }
+
+    protected function createThumbs()
+    {
+        if (is_readable($this->path->getPathFile())) {
+            foreach ($this->thumbs as $thumb => $config) {
+                $thumbPath = $this->path->getDirectory() . '/' . $this->getThumbFileName($this->path->getFile()->getName(), $thumb);
+                if (!is_file($thumbPath)) {
+                    $this->generateImageThumb($config, $thumbPath);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param array $config
+     * @param string $thumbPath
+     */
+    protected function generateImageThumb($config, $thumbPath)
+    {
+        $width = ArrayHelper::getValue($config, 'width');
+        $height = ArrayHelper::getValue($config, 'height');
+        $quality = ArrayHelper::getValue($config, 'quality', 100);
+        $mode = ArrayHelper::getValue($config, 'mode', ManipulatorInterface::THUMBNAIL_OUTBOUND);
+
+        if (!$width || !$height) {
+            $image = Image::getImagine()->open($this->path->getPathFile());
+            $ratio = $image->getSize()->getWidth() / $image->getSize()->getHeight();
+            if ($width) {
+                $height = ceil($width / $ratio);
+            } else {
+                $width = ceil($height * $ratio);
+            }
+        }
+        Image::thumbnail($this->path->getPathFile(), $width, $height, $mode)->save($thumbPath, ['quality' => $quality]);
+    }
+
+    /**
+     * @param string $file
+     * @param string $thumb
+     * @return string
+     */
+    protected function getThumbFileName($file, $thumb = 'thumb')
+    {
+        return $thumb . '-' . $file;
+    }
+
+    /**
+     * @param string $message
+     * @throws DomainException
+     */
+    public function domainException(string $message): void
+    {
+        throw new DomainException($message);
+    }
+}
